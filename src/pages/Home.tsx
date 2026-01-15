ab917a06e4d8d997bb6c39c04f56ee759d4bdb00
AHegeT <aheget11@gmail.com>
2026-01-14 18:18:14 -0600

    style: fix overflow x on mobile

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col font-sans selection:bg-primary/20">
+    <div className="min-h-screen flex flex-col font-sans selection:bg-primary/20 overflow-x-hidden">
       <Hero />
       <ValueChain />
 
